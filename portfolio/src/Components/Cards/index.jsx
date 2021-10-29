d8ccd0da63e87cff0acbb93837200b39dc945b67
Volpito <mpj.rigot@gmail.com>
2021-10-29 15:49:53 +0200

    Cards add

--- portfolio/src/Components/Cards/index.jsx
+++ portfolio/src/Components/Cards/index.jsx
@@ -39,17 +39,19 @@ const people = [
 ]
 
   return (
-    <div className="bg-current! mb-16 mt-6 sm:my-2">
+    <div className="bg-current! mb-16 ">
       <div className="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-24">
         <div className="space-y-8 sm:space-y-12">
-
+        <div className="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-8 lg:max-w-5xl">
+            <h2 className="text-6xl font-extrabold tracking-tight sm:text-8xl mb-12 sm:mb-24"><span className="text-blue-400">MPJ</span> RIGOT</h2>
+          </div>
             <ul
-              className="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-6"
+              className="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-4"
             >
             {people.map((person) => (
               <li key={person.name}>
                 <div className="space-y-4">
-                  <img className="mx-auto h-20 w-20 lg:w-24 lg:h-24" src={person.imageUrl} alt="" />
+                  <img className="rounded-3xl p-2 bg-gray-50 mx-auto h-20 w-20 lg:w-24 lg:h-24" src={person.imageUrl} alt="" />
                   <div className="space-y-2">
                     <div className="text-xs font-medium lg:text-sm">
                       <h3 className="text-blue-400">{person.name}</h3>
