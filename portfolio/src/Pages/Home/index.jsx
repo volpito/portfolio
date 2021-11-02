f3324182a3192ad24636e4e58b3630296724cf02
Volpito <mpj.rigot@gmail.com>
2021-11-02 19:17:33 +0100

    contact home fix

--- portfolio/src/Pages/Home/index.jsx
+++ portfolio/src/Pages/Home/index.jsx
@@ -22,7 +22,7 @@ const Home = () => {
       < SectionImage />
 
       <div className="absolute w-full h-9 bg-blue-400"><p></p></div>
-      <div className="sm:-pt-12 pt-12">
+      <div className="sm:-pt-12 pt-12 sm:block hidden">
         <h2 className="topCenter text-3xl font-extrabold sm:text-4xl mb-8 mt-12 text-blue-400">{translation(lang, 'FomIntroTitle')}</h2>
         <p className="topCenter mt-4 text-lg leading-6 text-gray-400">{translation(lang, 'FomIntroContent')}</p>
       </div> 
