02d98717cfe1fd17e9cac88cc6158d1b0cdb0f21
Volpito <mpj.rigot@gmail.com>
2021-11-08 15:40:04 +0100

    img vs mp4 fix

--- portfolio/src/Components/SectionImage/index.jsx
+++ portfolio/src/Components/SectionImage/index.jsx
@@ -11,18 +11,6 @@ export default function SectionAPI() {
     <>
     <div className="bg-fixed h-96 bg-top" style={{backgroundImage: `url(${sky2})`}}>
     </div>
-    <div className="max-w-2xl mx-auto text-center py-12 px-4 sm:px-6 lg:px-8 text-blue-400">
-      <h2 className="text-3xl font-extrabold sm:text-4xl mb-8">
-        <span className="block">      
-          {translation(lang, 'TitleIMG')}
-        </span>
-      </h2>
-      <p className="mt-4 text-lg leading-6 text-gray-400">
-        {translation(lang, 'ContentIMG')}
-      </p>
-    </div>
-    <div className="bg-fixed h-96 bg-top" style={{backgroundImage: `url(${sky})`}}>
-    </div>
     <div className="absolute w-full h-1 bg-gray-700"><p></p></div>
     <div className="sm:w-full mx-auto text-center py-12 px-4 sm:px-6 lg:px-8 text-blue-400 bg-gradient-to-b from-gray-700 to-black">
       <h2 className="text-3xl font-extrabold sm:text-4xl mb-8">
@@ -33,6 +21,8 @@ export default function SectionAPI() {
     </div>
     < BackgoundVideo />
     <div className="py-12 bg-gradient-to-b from-black via-black to-black"></div>
+    <div className="bg-fixed h-96 bg-top" style={{backgroundImage: `url(${sky})`}}>
+    </div>
     </>
   )
 }
