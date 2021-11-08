08a12443479ab850944dfe85ca848597128e22c3
Volpito <mpj.rigot@gmail.com>
2021-11-08 15:32:02 +0100

    smol scc fix

--- portfolio/src/Components/SectionImage/index.jsx
+++ portfolio/src/Components/SectionImage/index.jsx
@@ -23,7 +23,8 @@ export default function SectionAPI() {
     </div>
     <div className="bg-fixed h-96 bg-top" style={{backgroundImage: `url(${sky})`}}>
     </div>
-    <div className="sm:w-full mx-auto text-center py-12 px-4 sm:px-6 lg:px-8 text-blue-400 bg-gradient-to-b from-gray-800 to-black">
+    <div className="absolute w-full h-1 bg-gray-700"><p></p></div>
+    <div className="sm:w-full mx-auto text-center py-12 px-4 sm:px-6 lg:px-8 text-blue-400 bg-gradient-to-b from-gray-700 to-black">
       <h2 className="text-3xl font-extrabold sm:text-4xl mb-8">
         <span className="block">      
           {translation(lang, 'TitleVID')}
