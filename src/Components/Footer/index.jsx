bb593161faa8bb79d84f6a8bf3ee0c460ba0e225
RIGOT Maxime <69733938+volpito@users.noreply.github.com>
2025-03-11 10:58:35 +0100

    Update index.jsx
    Footer Update

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -65,7 +65,7 @@ export default function Footer() {
           ))}
         </div>
         <div className="mt-8 md:mt-0 md:order-1">
-          <p className="text-center text-base text-gray-400">&copy; 2022 Portfolio, MPJRigot -- Junior DevOps @Ubisoft Ivory Tower</p>
+          <p className="text-center text-base text-gray-400">&copy; 2025 Portfolio, MPJRigot -- DevOps @Ubisoft Ivory Tower</p>
         </div>
       </div>
     </footer>
